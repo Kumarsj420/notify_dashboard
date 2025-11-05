03f55992aeda277835bed8562b38d740a488dee4
makkysharma <ms9233025@gmail.com>
2025-11-05 12:33:51 +0530

    sidebar created

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -36,7 +36,7 @@ const userNavigation = [
 export default function Header() {
     const [sidebarOpen, setSidebarOpen] = useState(false)
     return (
-        <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-p-800 px-4 shadow-xs sm:gap-x-6 sm:px-6 lg:px-8">
+        <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-sc-200 bg-p-800 px-4 shadow-xs sm:gap-x-6 sm:px-6 lg:px-8">
             <button type="button" onClick={() => setSidebarOpen(true)} className="-m-2.5 p-2.5 text-gray-700 lg:hidden">
                 <span className="sr-only">Open sidebar</span>
                 <Bars3Icon aria-hidden="true" className="size-6" />
