77126204b4c8c80da7a73404f31d6246348949f9
Suraj <kumarsj420@gmail.com>
2025-11-06 19:19:22 +0530

    header color improved

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -98,7 +98,7 @@ export default function Header() {
                     </DialogPanel>
                 </div>
             </Dialog>
-            <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-sc-200 bg-p-800 px-4 shadow-xs sm:gap-x-6 sm:px-6 lg:px-8">
+            <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-sc-200 bg-linear-to-b from-p-800 to-p-950 f px-4 shadow-xs sm:gap-x-6 sm:px-6 lg:px-8">
                 <button type="button" onClick={() => setSidebarOpen(true)} className="-m-2.5 p-2.5 text-sc-700 lg:hidden">
                     <span className="sr-only">Open sidebar</span>
                     <Bars3Icon aria-hidden="true" className="size-6" />
